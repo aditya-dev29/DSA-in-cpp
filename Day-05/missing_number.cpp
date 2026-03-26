570ce251c1ef26f9ea2b285712863ac114089feb
AdityaKumar <raoaditya9461@gmail.com>
2026-03-26 23:24:52 +0530

    Added missing_number_int_array

---
+++ Day-05/missing_number.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+//brute force approch
+int missing_number(vector<int> &arr){
+        int n = arr.size() + 1;
+    for (int i = 1; i <= n; i++) {
+        bool found = false;
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] == i) {
+                found = true;
+                break;
+            }
+        }
+
+        // If the current number is not present
+        if (!found)
+            return i;
+    }
+    return -1;
+}
+int main() {
+    vector<int> arr = {8, 2, 4, 5, 3, 7, 1}; 
+    cout << missing_number(arr) << endl; 
+    return 0;
+}
