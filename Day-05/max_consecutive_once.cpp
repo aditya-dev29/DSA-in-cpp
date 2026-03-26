289eb83d506f5b4d49bc2bea9d6297224fa912be
AdityaKumar <raoaditya9461@gmail.com>
2026-03-26 23:38:17 +0530

    Added max consecutive_once

---
+++ Day-05/max_consecutive_once.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximum_consecutive(vector<int>& arr){
+    int maxi = 0;
+    int cnt = 0;
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i] == 1){
+            cnt++;
+            maxi = max(maxi, cnt);
+        }else{
+            cnt = 0;
+        }
+        
+    }
+    return maxi;
+}
+
+int main(){
+    vector<int> arr = {1,1,1,0,0,1,1,0,1,1,1,1,0};
+    cout<<maximum_consecutive(arr)<<endl;
+    return 0;
+}
